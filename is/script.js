d21be491bff40b92044a781f6c28cd2e91f53fe7
paventhan183 <paventhanbtech1990@gmail.com>
2025-08-19 21:05:55 +0530

    Update script.js

--- is/script.js
+++ is/script.js
@@ -18,10 +18,29 @@ menuToggle.addEventListener('keydown', (e) => {
     navMenu.classList.toggle('show');
   }
 });
+const counters = document.querySelectorAll('.counter');
+  const speed = 200; // lower = faster
+
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-target');
+      const count = +counter.innerText;
+      const increment = Math.ceil(target / speed);
+
+      if (count < target) {
+        counter.innerText = count + increment;
+        setTimeout(updateCount, 20);
+      } else {
+        counter.innerText = target + "+";
+      }
+    };
+    updateCount();
+  });
 
 // Banner slider functionality
 const slides = document.querySelectorAll('.banner-slider .slide');
 let currentIndex = 0;
+
 
 function showSlide(index) {
   slides.forEach((slide, i) => {
